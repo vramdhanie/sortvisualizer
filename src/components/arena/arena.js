c698788392766d94b8f5846d3f78dc7c1c345580
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-08-26 15:50:04 -0400

    Add arena component

---
+++ src/components/arena/arena.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Arena extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <div className="arena">
+                This is the Arena
+            </div>
+        )
+    }
+}
