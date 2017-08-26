b1f2c74d90d33b5154893180207d36fad47d639c
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-08-26 10:21:16 -0400

    Add basic reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import * as actions from '../actions';
+
+const initialState = {
+
+};
+
+export const reducer = (state=initialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+};
