0c01054f743763f805281ffc793b05b2c1564c96
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-08-26 15:50:56 -0400

    Add react router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Arena from '../arena/arena';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router>
+          <div className="App">
+              <Route path="/" exact={true} render={() => (
+                  <div className="App-header">
+                      <img src={logo} className="App-logo" alt="logo" />
+                      <h2>Welcome to Sort Visualizer</h2>
+                  </div>
+              )}/>
+            <Route path="/arena" component={Arena}/>
+            <Route path="/home" render={() => (
+                <div>
+                    Welcome to Sort Visualizer
+                </div>
+            )}/>
+          </div>
+        </Router>
     );
   }
 }
