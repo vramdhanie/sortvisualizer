dfeee129913564fa5666528980aa1fa880629a93
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-08-26 10:37:22 -0400

    Add a store

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import { reducer } from './reducers';
+
+export default createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(thunk)
+);
