669a0fd75e266d09e0fb0b5ecced62280e980814
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-08-29 18:30:00 -0400

    Add card component

---
+++ src/components/card/card.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './card.css';
+
+export default class Card extends Component {
+    render(){
+        <div className="card">
+            This is a card
+        </div>
+    }
+}
