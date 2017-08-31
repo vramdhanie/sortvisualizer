440b78214f2e6152c1fd1e71d7af980b461ce8b3
Vincent Ramdhanie <vramdhanie@gmail.com>
2017-08-30 22:31:27 -0400

    Add array component

--- src/components/app/App.test.js
+++ src/components/app/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow, mount } from 'enzyme';
+import AppBar from 'material-ui/AppBar';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('should render children', () => {
+  const rendered = shallow(<App/>);
+    expect(rendered.contains(<AppBar title="Sort Viz" iconClassNameRight="muidocs-icon-navigation-expand-more"/>)).toEqual(true);
+})
---
+++ src/components/array/array.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './array.css';
+
+export default class Array extends Component {
+    render(){
+        return (
+            <div className="array">
+                This is an array of cards
+            </div>
+        );
+    }
+}
